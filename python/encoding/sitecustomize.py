529a8dc67db6275105e39c1a782903712f439c40
liushuai <liu.shuai@icntv.tv>
2015-01-29 15:37:31 +0800

    Some tools

---
+++ python/encoding/sitecustomize.py
@@ -0,0 +1,8 @@
+'''
+Put this file into directory PYTHON_HOME/lib/site-packages/
+
+Invoke sys.getdefaultencoding(), we can get the right encoding.
+'''
+import sys
+
+sys.setdefaultencoding('utf-8')
